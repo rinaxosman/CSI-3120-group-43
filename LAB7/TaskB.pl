88153e2ea34b36db50685f85f18a8aa59e7cc677
Sameed Jafri <sjafr066@uottawa.ca>
2024-11-18 13:59:12 -0500

    adding taskB code

--- LAB7/TaskB.pl
+++ LAB7/TaskB.pl
@@ -2,3 +2,64 @@
 % - Alex Clements (300237898)
 % - Rina Osman (300222206)
 % - Sameed Jafri (300253861)
+
+% DCG for character descriptions
+character_description --> 
+    character_type(Type),
+    [','],
+    character_subtype(Type, Subtype),
+    [','],  
+    sequence_number,
+    [','],
+    movement_direction(Type),
+    [','],
+    health_level,
+    [','],
+    weapon(Type),
+    [','],
+    movement_style.
+
+% DCG for character type
+character_type(Type) --> 
+    [Type],
+    {member(Type, [hero, enemy])}.
+
+% DCG for character subtype
+character_subtype(enemy, Subtype) --> 
+    [Subtype],
+    {member(Subtype, [darkwizard, demon, basilisk])}.
+
+character_subtype(hero, Subtype) -->
+    [Subtype],
+    {member(Subtype, [wizard, mage, elf])}.
+
+% DCG for sequence number
+sequence_number --> 
+    [Number],
+    { integer(Number), Number > 0 }.
+
+% DCG for movement direction
+movement_direction(enemy) --> 
+    [towards].
+
+movement_direction(hero) -->
+    [towards];
+    [away].
+
+% DCG for health level
+health_level --> 
+    [Level],
+    { member(Level, [very_weak, weak, normal, strong, very_strong]) }.
+
+% DCG for weapon
+weapon(hero) --> 
+    [has_Weapon];
+    [no_weapon].
+
+weapon(enemy) -->
+    [no_weapon].
+
+% DCG for movement style
+movement_style --> 
+    [Style],
+    { member(Style, [jerky, stealthy, smoothly]) }.
